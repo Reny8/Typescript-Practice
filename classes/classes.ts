467bef5d219b5b7ec739bcff3584db84b9d153cc
Reyes <reny.reyes@accenture.com>
2023-03-02 13:30:14 -0500

    Practice for constructors + super()

--- classes/classes.ts
+++ classes/classes.ts
@@ -1,11 +1,19 @@
 class Vehicle {
+    constructor(public color: string) { }
+
     protected honk(): void {
         console.log("Beep")
     }
 }
 
+const vehicle = new Vehicle('orange')
+console.log(vehicle.color)
+
 // BASIC INHERITANCE
 class Car extends Vehicle {
+    constructor(color: string, public wheels: number) {
+        super(color)
+    }
     // OVERRIDING FUNCTION FOR THE CHILD CLASS
     private drive(): void {
         console.log("You are now driving")
@@ -17,5 +25,5 @@ class Car extends Vehicle {
     }
 }
 
-const car = new Car()
+const car = new Car('red', 4)
 car.startDrivingProcess()
