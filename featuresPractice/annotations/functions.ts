5c1ef441c7eedbe3e04d76de3ab1069d81e16ae7
Reyes <reny.reyes@accenture.com>
2023-03-02 09:36:57 -0500

    Using the never annotation example

--- featuresPractice/annotations/functions.ts
+++ featuresPractice/annotations/functions.ts
@@ -8,3 +8,13 @@ function divide(a:number, b:number): number {
 return a / b
 }
 
+// USING VOID NO RETURN TYPE
+const logger = (message: string): void => {
+    console.log(message)
+    // can return null and undefined
+}
+
+// USING NEVER MEANS THAT WE ARE NEVER GOING TO ACTUALLY REACH TO THE END OF THE FUNCTION WITHOUT RETURNING A VALUE
+const throwError = (message: string): never=> {
+    throw new Error(message)
+}
