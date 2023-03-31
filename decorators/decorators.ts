9d626146aca37d7bc69860a2b9aa4c53883786d5
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-03-31 16:50:40 -0400

    Add decorator example

---
+++ decorators/decorators.ts
@@ -0,0 +1,26 @@
+class Boat {
+  color: string = "red";
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+  @logError("Oops there was an error")
+  pilot(): void {
+    throw new Error();
+    console.log("swish");
+  }
+}
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+    desc.value = function () {
+      try {
+        method();
+      } catch (error) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
+
+new Boat().pilot();
